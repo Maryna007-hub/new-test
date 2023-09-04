698ba019cd98574622edcc4eb2b2e35382b10aee
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-04 14:43:48 +0100

    upd

--- src/App.js
+++ src/App.js
@@ -4,9 +4,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-  Weather app
-    </div>
-  );
+    
+    <div className="container">
+   
+    <Weather defaultCity="Lisbon"/>
+     <footer>
+       This project was coded by 
+        <a href='/'> Maryna Holinka </a>
+        and is <a href='https://github.com/Maryna007-hub/digital-ocean-tutorial'
+       target="_blank"
+       rel="noopener noreferrer">
+  open-soursed on GitHub
+     </a>
+     </footer>
+     </div>
+ </div>
+);
 }
+
 
 
