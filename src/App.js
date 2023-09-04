029d6c451a174335b18c761d532e54166fef9685
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-04 14:52:57 +0100

    upd

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-    
-    <div className="container">
-   
-    <Weather defaultCity="Lisbon"/>
-     <footer>
-       This project was coded by 
-        <a href='/'> Maryna Holinka </a>
-        and is <a href='https://github.com/Maryna007-hub/digital-ocean-tutorial'
-       target="_blank"
-       rel="noopener noreferrer">
-  open-soursed on GitHub
-     </a>
-     </footer>
-     </div>
- </div>
-);
-}
+  
+    <h1>Lisbon💛 </h1>
+       <footer>
+          This project was coded by 
+           <a href='/'> Maryna Holinka </a>
+           and is <a href='https://github.com/Maryna007-hub/digital-ocean-tutorial'
+          target="_blank"
+          rel="noopener noreferrer">
+     open-soursed on GitHub
+        </a>
+        </footer>
+        </div>
 
+  );
+ }
+
+    
+   
 
 
