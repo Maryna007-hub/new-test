22223defa68d762231ee3a2ad88d05ec74cc8e5c
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-04 15:30:17 +0100

    upd

--- src/Weather.js
+++ src/Weather.js
@@ -2,12 +2,22 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-    return <div className="Weather">
-    <h1>Lisbon</h1>
-    <ul>
+    return (
+    <div className="Weather">
+     <h1>Lisbon</h1>
+     <ul>
         <li>Monday 20:00</li>
         <li>Clear</li>
-        
-    </ul>
-    </div>;
+     </ul>
+     <div className="row">
+       <div className="col-6">
+        <img src="" alt="Clear"/> 18C|F 
+       </div>
+       <div className="col-6">
+       <li>Humidity: 37%</li>
+        <li>Wind: 23 km/h</li>
+       </div>
+     </div>
+    </div>
+    );
 }
