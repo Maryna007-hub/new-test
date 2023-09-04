26ab358a65b03334b72e043f846d265f75bdf696
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-04 15:16:52 +0100

    upd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
  <div className="container">
     <h1>Lisbon💛 </h1>
+    <Weather/>
     <button className="btn btn-primary">Hellonp</button>
        <footer>
           This project was coded by 
