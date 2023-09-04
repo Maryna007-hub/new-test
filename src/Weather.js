74718bd4a2f1400e6fb67e3572746c8813088656
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-04 15:40:44 +0100

    upd

--- src/Weather.js
+++ src/Weather.js
@@ -4,6 +4,18 @@ import "./Weather.css";
 export default function Weather() {
     return (
     <div className="Weather">
+        <form>
+            <div className="row">
+                <div className="col-9">
+            <input type="seatch" placeholder="Enter a city" 
+            className="form-control"/>
+            </div>
+            <div className="col-3">
+            <input type="submit" value="Searth" 
+            className="btn btn-primary"/>
+             </div>
+            </div>
+        </form>
      <h1>Lisbon</h1>
      <ul>
         <li>Monday 20:00</li>
@@ -11,7 +23,8 @@ export default function Weather() {
      </ul>
      <div className="row">
        <div className="col-6">
-        <img src="" alt="Clear"/> 18C|F 
+        <img src="https://cdn-icons-png.flaticon.com/128/4814/4814268.png"
+         alt="Clear"/> 18C|F 
        </div>
        <div className="col-6">
        <li>Humidity: 37%</li>
